8cd6da5268bceb4c4732b78b70f76956c3178382
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-03-25 13:34:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,8 @@
+// Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
     int count1 = 0;
     for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++){
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] >= 0 && arr[j] >= 0 && (arr[i] + arr[j]) == value)
                 count1++;
         }
@@ -12,15 +13,13 @@ int countPairs2(int *arr, int len, int value) {
     int count1 = 0;
     int l = 0;
     int r = len - 1;
-    while (l != r)
-    {
+    while (l != r) {
         int sum = arr[l] + arr[r];
         if (sum < value)
             l++;
         else if (sum > value)
             r--;
-        else
-        {
+        else {
             count1++;
             l++;
         }
@@ -29,32 +28,29 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
     int count1 = 0;
-    int right = len - 1;
-    int left = 0;
-    while (left < right - 1) {
-        int center = (left + right) / 2;
-    if (arr[center] > value)
-        right = center;
-    else
-        left = center;
-    }
-    len = right - 1;
-    for (int i = 0; i < len; i++) {
-        left = i + 1;
-        right = len - 1;
-        int count2 = 0;
-        while (left < right) {
-            int center2 = (left + right) / 2;
-            if (arr[center2] < (value - arr[i]))
-                left = center2 + 1;
+    for (int i = 0; i < len - 1; i++) {
+        int left = i, right = len;
+        while (left < right - 1) {
+            int center = (left + right) / 2;
+            if (i != center && arr[i] + arr[center] == value) {
+                count1 += 1;
+                for (int j = center + 1; j < right
+                    && arr[i] + arr[j] == value
+                    && center != i; ++j) {
+                    count1 += 1;
+                }
+                for (int j = center - 1; j > left
+                    && arr[i] + arr[j] == value
+                    && center != i; --j) {
+                    count1 += 1;
+                }
+                break;
+            }
+            if (arr[i] + arr[center] > value)
+                right = center;
             else
-                right = center2;
+                left = center;
         }
-        while (arr[left] == (value - arr[i])) {
-            count2++;
-            left++;
-        }
-        count1 += count2;
     }
     return count1;
 }
